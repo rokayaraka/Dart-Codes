fe13abd86a82452eec27e30c4fb2451dcb6faba8
rokayaraka <rokayaraka251@gmail.com>
2025-10-17 19:20:47 +0600

    customized folder

---
+++ Course Dart Programmas/Module 1 -- Dart Basics/01_hello_world.dart
@@ -0,0 +1,10 @@
+void main() {
+  print('Hello, World!');
+
+  bool isActive=false;
+  print("Status: $isActive");
+
+  String name = "Shaon"; 
+print("Hello, ${name.toUpperCase()}!");
+double x = 5; double y = 2; print(x / y);
+}
---
+++ Course Dart Programmas/Module 1 -- Dart Basics/hello_world.dart
@@ -0,0 +1,19 @@
+void main()
+{
+
+  // String name="raka";
+  // int age=25;
+  // //print(name); 
+  // print("Hello World, $name");
+  // print('your are $age years old');
+
+  // int count = 112;
+  // double price=112.22;
+
+  // bool isActive=true;
+
+  String? fullname;
+  fullname='raka';
+  print(fullname!);
+  
+}
